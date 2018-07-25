359a82f229ed9545f985b255bdc186ba8b68821f
Zachary Balton <Zachary_Balton@nt02.unitedway.org>
2018-07-25 08:03:02 -0400

    Adding Subtheme, header, footer, login page.

---
+++ sites/all/themes/uwwpartners/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function uwwpartners_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function uwwpartners_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function uwwpartners_preprocess_node(&$variables) {
+}
---
+++ sites/all/themes/uwwpartners/templates/page.tpl.php
@@ -0,0 +1,362 @@
+<!--.page -->
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta property="og:image" content="https://www.unitedway.org/i/images/united-way-worldwide-fb-share.png/@zx_1200@zy_630">
+<meta name="google-site-verification" content="AhOgHE3bN-jlGKpE0vLSc7cK3KQ8BWPgDLP17BNpTsU" />
+
+<title>United Way Worldwide</title>
+
+
+<link rel="icon" href="https://www.unitedway.org/assets/img/meta/favicon16.png" sizes="16x16">
+<link rel="icon" href="https://www.unitedway.org/assets/img/meta/favicon32.png" sizes="32x32">
+<link rel="icon" href="https:/www./unitedway.org/assets/img/meta/favicon64.png" sizes="64x64">
+<link rel="shortcut icon" type="image/x-icon" href="https:/www./unitedway.org/favicon48.ico?2">
+<link rel="apple-touch-icon-precomposed" href="https://www.unitedway.org/assets/img/meta/apple-touch-icon-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="https://www.unitedway.org/assets/img/meta/apple-touch-icon-72x72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="https://www.unitedway.org/assets/img/meta/apple-touch-icon-114x114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="https://www.unitedway.org/assets/img/meta/apple-touch-icon-144x144-precomposed.png">
+<meta name="apple-mobile-web-app-title" content="United Way">
+
+<!--[if lte IE 9 ]><link rel="stylesheet" href="https://www.unitedway.org/assets/css/split/styles.css"><![endif]-->
+<!--<link rel="stylesheet" href="https://www.unitedway.org/assets/css/styles.css">-->
+
+<link rel="stylesheet" href="https://secure.unitedway.org/page/-/css/reskin.css"/>
+
+
+
+<!--[if (gt IE 9)|!(IE)]><!--><script type="text/javascript">var MTIConfig = { "EnableCustomFOUTHandler" : true, "RemoveMTIClass" : true};</script><!--<![endif]-->
+
+<script src="https://www.unitedway.org/assets/js/vendor/modernizr.js"></script>
+
+<script src="https://use.typekit.net/man0led.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+
+
+<script async src="//analytics.newscred.com/analytics_7d0496beadfb488090fe5fd67065b6b2.js"></script>
+
+<style>
+
+body > p {
+    display: none;
+}
+
+</style>
+
+
+</head>
+
+<div role="document" class="page">
+
+  <!--.l-header -->
+  <div class="site-container">
+
+        <div class="header-logo">
+            <a href="https://www.unitedway.org"><img src="https://www.unitedway.org/assets/img/new-logo.svg" alt="United Way Logo"></a>
+        </div>
+        
+        <header class="desktop-header show-for-large-up">
+            <nav class="utility-menu">
+                <ul class="right">                                
+                    <li class="utility-menu__volunteer">
+                        <a href="https://www.unitedway.org/get-involved/volunteer/">Volunteer</a>
+                    </li>
+ 
+                    <li class="utility-menu__211">
+                        <a href="http://211.org/" target="_blank">2-1-1</a>
+                    </li>
+                    <li class="utility-menu__locate">
+                        <a href="https://www.unitedway.org/find-your-united-way/">Find Your United Way</a>
+                    </li>
+                    
+                    <li class="utility-menu__donate">
+                        <a href="https://www.unitedway.org/get-involved/ways-to-give/donate">Donate</a>
+                    </li>
+                </ul>
+            </nav> <!--/.utility-menu-->
+   
+            <nav class="main-menu">
+                <ul class="right">
+                    <li class="main-menu__parent-item">
+                        <a href="#">Videos</a>
+                    </li>
+                    <li class="main-menu__parent-item">
+                        <a href="#">Infographics</a>
+                    </li>
+                    <li class="main-menu__parent-item">
+                        <a href="#">Impact</a>
+                    </li>
+                    <li class="main-menu__parent-item">
+                        <a href="#">Employee Engagement</a>
+                    </li>
+                    <li class="main-menu__parent-item">
+                        <a href="#">Logos</a>
+                    </li>
+                    <li class="main-menu__parent-item">
+                        <a href="#">Images</a>
+                    </li>
+                    <li class="main-menu__parent-item">
+                        <a href="#">Brand Standards</a>
+                    </li>
+                    <!--<li class="main-menu__btns">
+                        <a href="#">Engagement</a>
+                    </li>-->
+                </ul>
+            </nav> <!--/.main-menu-->        
+        </header>  
+
+        <header class="mobile-header show-for-medium-down">
+            <ul class="mobile-header__utilities">
+            
+               
+                <li class="mobile-header__menu-toggle">
+                    <a href="#" class="js-expand-nav"><span>MENU</span></a>
+                </li>
+            
+            </ul>
+        </header>
+       
+        <nav class="mobile-menu">
+
+<ul class="mobile-menu__root"><li class="mobile-menu__level-1">          
+            <div class="mobile-menu__parent-group">
+                <a href="#" class="js-expand-submenu expand-level-1"><span class="menu-title">Video/span><span class="menu-icon">&times;</span></a>
+            </div>   
+
+    </li><li class="mobile-menu__level-1">        
+            <div class="mobile-menu__parent-group">
+                <a href="#" class="js-expand-submenu expand-level-1"><span class="menu-title">Infographics</span><span class="menu-icon">&times;</span></a>
+            </div>       
+    </li><li class="mobile-menu__level-1">          
+            <div class="mobile-menu__parent-group">
+                <a href="#" class="js-expand-submenu expand-level-1"><span class="menu-title">Impact</span><span class="menu-icon">&times;</span></a>
+            </div>   
+    </li><li class="mobile-menu__level-1">
+                  <div class="mobile-menu__parent-group">
+                <a href="#" class="js-expand-submenu expand-level-1"><span class="menu-title">Employee Engagement</span><span class="menu-icon">&times;</span></a>
+            </div>       
+    </li>  
+    <li class="mobile-menu__level-1">
+                    <div class="mobile-menu__parent-group">
+                <a href="#" class="js-expand-submenu expand-level-1"><span class="menu-title">Logos</span><span class="menu-icon">&times;</span></a>
+            </div>
+    </li> 
+     <li class="mobile-menu__level-1">
+                    <div class="mobile-menu__parent-group">
+                <a href="#" class="js-expand-submenu expand-level-1"><span class="menu-title">Images</span><span class="menu-icon">&times;</span></a>
+            </div>
+    </li>
+    <li class="mobile-menu__level-1">
+                    <div class="mobile-menu__parent-group">
+                <a href="#" class="js-expand-submenu expand-level-1"><span class="menu-title">Brand Standards</span><span class="menu-icon">&times;</span></a>
+            </div>
+    </li>
+</ul>
+
+</nav> <!--/.main-menu-->
+  <!--/.l-header -->
+
+  <?php if (!empty($page['featured'])): ?>
+    <!--.l-featured -->
+    <section class="l-featured row">
+      <div class="columns">
+        <?php print render($page['featured']); ?>
+      </div>
+    </section>
+    <!--/.l-featured -->
+  <?php endif; ?>
+
+  <?php if ($messages && !$zurb_foundation_messages_modal): ?>
+    <!--.l-messages -->
+    <section class="l-messages row">
+      <div class="columns">
+        <?php if ($messages): print $messages; endif; ?>
+      </div>
+    </section>
+    <!--/.l-messages -->
+  <?php endif; ?>
+
+  <?php if (!empty($page['help'])): ?>
+    <!--.l-help -->
+    <section class="l-help row">
+      <div class="columns">
+        <?php print render($page['help']); ?>
+      </div>
+    </section>
+    <!--/.l-help -->
+  <?php endif; ?>
+
+  <!--.l-main -->
+  <main role="main" class="row l-main">
+    <!-- .l-main region -->
+    <div class="<?php print $main_grid; ?> main columns">
+      <?php if (!empty($page['highlighted'])): ?>
+        <div class="highlight panel callout">
+          <?php print render($page['highlighted']); ?>
+        </div>
+      <?php endif; ?>
+
+      <a id="main-content"></a>
+
+      <?php if ($breadcrumb): print $breadcrumb; endif; ?>
+
+      <?php if ($title): ?>
+        <?php print render($title_prefix); ?>
+        <h1 id="page-title" class="title"><?php print $title; ?></h1>
+        <?php print render($title_suffix); ?>
+      <?php endif; ?>
+
+      <?php if (!empty($tabs)): ?>
+        <?php print render($tabs); ?>
+        <?php if (!empty($tabs2)): print render($tabs2); endif; ?>
+      <?php endif; ?>
+
+      <?php if ($action_links): ?>
+        <ul class="action-links">
+          <?php print render($action_links); ?>
+        </ul>
+      <?php endif; ?>
+
+      <?php print render($page['content']); ?>
+    </div>
+    <!--/.l-main region -->
+
+    <?php if (!empty($page['sidebar_first'])): ?>
+      <aside role="complementary" class="<?php print $sidebar_first_grid; ?> sidebar-first columns sidebar">
+        <?php print render($page['sidebar_first']); ?>
+      </aside>
+    <?php endif; ?>
+
+    <?php if (!empty($page['sidebar_second'])): ?>
+      <aside role="complementary" class="<?php print $sidebar_sec_grid; ?> sidebar-second columns sidebar">
+        <?php print render($page['sidebar_second']); ?>
+      </aside>
+    <?php endif; ?>
+  </main>
+  <!--/.l-main -->
+
+  <?php if (!empty($page['triptych_first']) || !empty($page['triptych_middle']) || !empty($page['triptych_last'])): ?>
+    <!--.triptych-->
+    <section class="l-triptych row">
+      <div class="triptych-first medium-4 columns">
+        <?php print render($page['triptych_first']); ?>
+      </div>
+      <div class="triptych-middle medium-4 columns">
+        <?php print render($page['triptych_middle']); ?>
+      </div>
+      <div class="triptych-last medium-4 columns">
+        <?php print render($page['triptych_last']); ?>
+      </div>
+    </section>
+    <!--/.triptych -->
+  <?php endif; ?>
+
+  <?php if (!empty($page['footer_firstcolumn']) || !empty($page['footer_secondcolumn']) || !empty($page['footer_thirdcolumn']) || !empty($page['footer_fourthcolumn'])): ?>
+    <!--.footer-columns -->
+    <section class="row l-footer-columns">
+      <?php if (!empty($page['footer_firstcolumn'])): ?>
+        <div class="footer-first medium-3 columns">
+          <?php print render($page['footer_firstcolumn']); ?>
+        </div>
+      <?php endif; ?>
+      <?php if (!empty($page['footer_secondcolumn'])): ?>
+        <div class="footer-second medium-3 columns">
+          <?php print render($page['footer_secondcolumn']); ?>
+        </div>
+      <?php endif; ?>
+      <?php if (!empty($page['footer_thirdcolumn'])): ?>
+        <div class="footer-third medium-3 columns">
+          <?php print render($page['footer_thirdcolumn']); ?>
+        </div>
+      <?php endif; ?>
+      <?php if (!empty($page['footer_fourthcolumn'])): ?>
+        <div class="footer-fourth medium-3 columns">
+          <?php print render($page['footer_fourthcolumn']); ?>
+        </div>
+      <?php endif; ?>
+    </section>
+    <!--/.footer-columns-->
+  <?php endif; ?>
+
+  <!--.l-footer -->
+<footer class="page-footer"> 
+        <div class="nav-container row">
+            <div class="page-footer__logo hide-for-medium-down large-2 columns">
+                <a href="https://www.unitedway.org/">
+                    <img src="https://www.unitedway.org/assets/img/new-logo.svg" alt="United Way Logo">
+                </a>
+            </div>
+
+            <nav class="page-footer__menu small-7 large-6 columns">
+                
+<ul><li class="nav-item-1">
+                        <a href="#">Partner Resources</a>
+<ul class="children"><li class="nav-item-2">
+                        <a href="#">Video</a>
+                    </li><li class="nav-item-3">
+                        <a href="#">Infographics</a>
+                    </li><li class="nav-item-4">
+                        <a href="#">Impact Collateral</a>
+                    </li><li class="nav-item-5">
+                        <a href="#">Employee Engagement</a>
+                    </li>
+                    <li class="nav-item-6">
+                        <a href="#">Logos</a>
+                    </li>
+                    <li class="nav-item-7">
+                        <a href="#">Images</a>
+                    </li>
+                    <li class="nav-item-8">
+                        <a href="#">Partner Brand Standards</a>
+                    </li>
+</ul>
+                    </li><li class="nav-item-9">
+                        <a href="#">Contact Us</a>
+
+                    </li><li class="nav-item-10">
+                        <a href="https://www.unitedway.org/privacy-policy">Privacy Policy</a>
+                    </li>
+                
+</ul>
+            </nav>
+            
+        <div class="page-footer__social small-5 medium-4 large-4 columns">
+                <h4 class="page-footer__heading">Social</h4>
+                <ul class="page-footer__social-icons">
+                    
+                    <li class="facebook-link">
+                        <a href="https://www.facebook.com/UnitedWay" target="_blank">Facebook</a>
+                    </li>               
+                    
+                    <li class="twitter-link">
+                        <a href="https://twitter.com/unitedway" target="_blank">Twitter</a>
+                    </li>
+                    
+                    <li class="youtube-link">
+                        <a href="https://www.youtube.com/unitedway" target="_blank">YouTube</a>
+                    </li>
+
+                    <li class="instagram-link">
+                        <a href="https://instagram.com/unitedway/" target="_blank">Instagram</a>
+                    </li> 
+
+                    <li class="linkedin-link">
+                        <a href="https://www.linkedin.com/company/1511592?trk=tyah&trkInfo=tarId%3A1407787780806%2Ctas%3Aunited%20way%20wo%2Cidx%3A2-1-2" target="_blank">LinkedIn</a>
+                    </li>  
+
+                    <li class="facebook-link">
+                        <a href="https://www.unitedway.org/blog/rss" target="_blank">Subscribe to our Blog</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div> <!--/.row-->
+    </footer>
+  <!--/.l-footer -->
+
+  <?php if ($messages && $zurb_foundation_messages_modal): print $messages; endif; ?>
+</div>
+<!--/.page -->
